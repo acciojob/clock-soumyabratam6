3d5749ee53f1f99b4c0364904ff1ee649485769b
soumyabratam6 <144446453+soumyabratam6@users.noreply.github.com>
2024-05-03 21:18:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+ const timerElement = document.getElementById('timer');
+let currentDate = new Date();
+setInterval(
+	function () {
+		currentDate = new Date();
+		timerElement.innerHTML = currentDate.toLocaleString();
+	},1000
+)
